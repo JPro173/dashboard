c7bfada2ceb30881feb68d76f0442e52c8db9c5e
Kostya Cholak <kostya.cholak@anvileight.com>
2017-02-08 17:39:31 +0200

    Create home view

--- notes_module/views.py
+++ notes_module/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from notes.models import Note
+
+
+class NotesListView(ListView):
+    model = Note
